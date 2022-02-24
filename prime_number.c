8c48e17dbcaf6d66738b3d787ee1507ac18a47f7
ramesh-sindhu <100334778+ramesh-sindhu@users.noreply.github.com>
2022-02-24 11:08:44 +0530

    #include <stdio.h> int main() {     int a,b,i,n;     scanf("%d",&i);
            if(i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0)
            printf("prime");
            else
            printf("not a prime");
        
    }

---
+++ prime_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i,n;
+    scanf("%d",&i);
+    
+    
+        if(i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0)
+        printf("prime");
+        else
+        printf("not a prime");
+    
+}
